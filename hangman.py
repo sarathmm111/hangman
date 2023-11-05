dbac880abdc90352f47eed1cb56e375b6b6d13ae
sarath <sarathmm224@gmail.com>
2023-11-05 23:10:37 +0530

    GREEN:added maskword function

--- hangman.py
+++ hangman.py
@@ -14,3 +14,14 @@ def get_random_word(wordlist="/usr/share/dict/words"):
             good_words.append(word)
 
         return random.choice(good_words)
+        
+def get_mask_word(word, guesses):
+    """Returns the provided word with all
+    letters except the ones in guesses replaced by -"""
+    ret = []
+    for i in word:
+        if i in guesses:
+            ret.append(i)
+        else:
+            ret.append("-")
+    return "".join(ret)
